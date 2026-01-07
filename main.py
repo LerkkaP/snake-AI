78570ed506a558647c0bbef8df5b38a1fbf2329a
Erik Peteri <peteri.erik@gmail.com>
2026-01-07 15:01:00 +0200

    center snake on screen

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
+class Snake():
+    def __init__(self):
+        self.size = 10
+        self.rect = pygame.Rect(0, 0, self.size, self.size)
+        self.rect.center = WIDTH // 2, HEIGHT // 2
+        self._color = "white"
+
+    def draw_snake(self):
+        pygame.draw.rect(screen, self._color, self.rect)
+
+snake = Snake()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,7 +27,7 @@ while running:
 
     screen.fill("black")
 
-
+    snake.draw_snake()
     pygame.display.flip()
 
     clock.tick(60)
